067956db4db237cbb0f6afc962e97ed50a28c3a1
gabriel-magioli <gabgomes@id.uff.br>
2026-09-17 19:27:37 -0300

    script BDEM - SINASC - tarefas 1 a 3

--- script_BDEM.R
+++ script_BDEM.R
@@ -241,7 +241,9 @@ names(dados_sinasc_1)
 # 31: 253520    32: 53413     33: 219129    35: 601437     
 # 41: 155066    42: 95313     43: 141411
 # 50: 42432     51: 53531     52: 95563     53: 43340 
-
+dados_sinasc_2 = dados_sinasc_1[substr(as.character(dados_sinasc_1$CODMUNRES), 1, 2) == "23",]
+View(dados_sinasc_2)
+dim(dados_sinasc_2)
 
 # Ao terminar a Tarefa 3 commit com a mensagem "script BDEM - SINASC - tarefas 1 a 3" e envie para o repositório Projeto_BDEM_2016
 
